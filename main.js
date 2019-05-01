6fc37bb2851a0d23bde460d67c2e23009bc8b0c6
manojvicky <manoj.save.earth@gmail.com>
2019-05-02 02:15:14 +0530

    data grid

---
+++ main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div className="app">
+      Datagrid
+    </div>
+  );
+}
+
+
+ReactDOM.render(
+    <App />, document.getElementById('app')
+);
